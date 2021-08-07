c197ffc6da0ba48ed873fc125139837488050cf0
@thalesbruno <thalesbrno@gmail.com>
2021-08-07 12:52:20 -0300

    [aula 9] Add Grid component

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Button from "../src/components/common/Button";
 import Footer from "../src/components/common/Footer";
 import Menu from "../src/components/common/Menu";
 import Text from "../src/components/foundation/Text";
+import { Grid } from "../src/components/layout/Grid";
 
 export default function Home() {
   return (
@@ -17,30 +18,46 @@ export default function Home() {
       }}
     >
       <Menu />
-      <div>
-        <Text tag="h1" variant="title" textAlign={{ xs: "center", md: "left" }}>
-          Compartilhe momentos e conecte-se com amigos
-        </Text>
-        <Text
-          tag="p"
-          variant="paragraph2"
-          textAlign={{ xs: "center", md: "left" }}
-        >
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industrys standard dummy text ever
-          since the 1500s.
-        </Text>
-        <Button
-          margin={{
-            xs: "auto",
-            md: "initial",
-          }}
-          display="block"
-          variant="primary.main"
-        >
-          Cadastrar
-        </Button>
-      </div>
+      <Grid.Container>
+        <Grid.Row>
+          <Grid.Col value={{ xs: 12, md: 5 }} offset={{ xs: 0, md: 1 }}>
+            <div>
+              <Text
+                tag="h1"
+                variant="title"
+                textAlign={{ xs: "center", md: "left" }}
+              >
+                Compartilhe momentos e conecte-se com amigos
+              </Text>
+              <Text
+                tag="p"
+                variant="paragraph2"
+                textAlign={{ xs: "center", md: "left" }}
+              >
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industrys standard dummy text
+                ever since the 1500s.
+              </Text>
+              <Button
+                margin={{
+                  xs: "auto",
+                  md: "initial",
+                }}
+                display="block"
+                variant="primary.main"
+              >
+                Cadastrar
+              </Button>
+            </div>
+          </Grid.Col>
+          <Grid.Col value={{ xs: 12, md: 5 }}>
+            <img
+              style={{ display: "block", margin: "auto" }}
+              src="https://bootcamp-alura-01-git-modulo01.omariosouto.vercel.app/images/phones.png"
+            />
+          </Grid.Col>
+        </Grid.Row>
+      </Grid.Container>
       <Footer />
     </div>
   );
