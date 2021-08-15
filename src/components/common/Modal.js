67bb3ab47850b27edf475d8132aa21555d1b2299
@thalesbruno <thalesbrno@gmail.com>
2021-08-15 11:20:06 -0300

    Add framer-motion animation to Modal component

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -1,19 +1,23 @@
 import styled, { css } from 'styled-components';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { motion } from 'framer-motion';
 
 const ModalWrapper = styled.div`
-  background-color: rgba(0,0,0,0.1);
+  background-color: rgba(0,0,0,0.3);
   position: fixed;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
   display: flex;
-  flex-direction: column;
+  /* flex-direction: column;   */
+  /* justify-content: flex-end; */
+  z-index: 10;
   align-items: stretch;
   margin: auto;
   overflow: scroll;
+  /* transition: .3s; */
   ${({ isOpen }) => {
     if (isOpen) {
       return css`
@@ -38,9 +42,29 @@ const Modal = ({ isOpen, onClose, children }) => (
       }
     }}
   >
-    { children({
-      'data-modal-safe-area': 'true',
-    }) }
+    <motion.div
+      variants={{
+        hidden: {
+          x: '100%',
+        },
+        visible: {
+          x: 0,
+          transition: {
+            duration: 0.3,
+          },
+        },
+      }}
+      animate={isOpen ? 'visible' : 'hidden'}
+      style={{
+        display: 'flex',
+        flex: 1,
+        justifyContent: 'flex-end',
+      }}
+    >
+      { children({
+        'data-modal-safe-area': 'true',
+      }) }
+    </motion.div>
   </ModalWrapper>
 );
 
