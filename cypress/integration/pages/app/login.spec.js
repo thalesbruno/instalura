35d1ca72909e9a4dd9e9dbd8b029d8bb990ffab2
@thalesbruno <thalesbrno@gmail.com>
2021-09-18 09:36:53 -0300

    test: update login test

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -2,10 +2,17 @@
 
 describe('/pages/app/login', () => {
   it('Fill the form and go to /app/profile/ page', () => {
+    cy.intercept('https://instalura-api-omariosouto.vercel.app/api/login').as('getToken');
     cy.visit('/app/login/');
-    cy.get('#formCadastro input[name="user"]').type('thales');
-    cy.get('#formCadastro input[name="password"]').type('senha');
+    cy.get('#formCadastro input[name="username"]').type('yazan');
+    cy.get('#formCadastro input[name="password"]').type('senhasegura');
     cy.get('#formCadastro button[type="submit"]').click();
     cy.url().should('include', '/app/profile');
+    cy.wait('@getToken')
+      .then((interception) => {
+        const { token } = interception.response.body.data;
+        cy.getCookie('APP_TOKEN').should('exist')
+          .then((c) => c === token);
+      });
   });
 });
